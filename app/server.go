7c0679e3509e1c9ef764811acbf90d83b931e8a3
Keshav <krkeshav123ytb@gmail.com>
2023-12-03 19:37:55 +0530

    stage 2

--- app/server.go
+++ app/server.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	// close the connection after everything is handled
+	defer connection.Close()
+
+	// dummy http response sent through connection
+	dummyMessage := "HTTP/1.1 200 OK\r\n\r\n"
+	connection.Write([]byte(dummyMessage))
 }
